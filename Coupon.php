915ba766d12ecfca3f66ec4a50c0272fc29b6c40
Duyet <duyet1805@gmail.com>
2025-06-09 20:16:47 +0700

    bug

--- Coupon.php
+++ Coupon.php
@@ -21,9 +21,10 @@ class Coupon extends Model
     public function calculateDiscount($orderAmount)
 {
     if ($this->type === 'percent') {
-        return $orderAmount * ($this->value / 100);
+        return $orderAmount * ($this->value / 100)
     }
     return min($this->value, $orderAmount);
 }
+
 
 }

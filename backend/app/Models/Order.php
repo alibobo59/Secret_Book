157b5417c7738ff97d24e4e9a58c0398e79e2606
alibobo59 <nguyentamminh.ntm@gmail.com>
2025-06-23 19:27:54 +0700

    order, oderitem, addresses migration and model

---
+++ backend/app/Models/Order.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'order_number',
+        'subtotal',
+        'shipping',
+        'total',
+        'status',
+        'notes',
+    ];
+
+    protected $casts = [
+        'status' => 'string',
+        'subtotal' => 'integer',
+        'shipping' => 'integer',
+        'total' => 'integer',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function address()
+    {
+        return $this->hasOne(Address::class);
+    }
+}

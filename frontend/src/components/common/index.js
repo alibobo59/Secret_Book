b449ea24c1f551662855086caca602d7d1a439e4
Huong <nthuong.su20@gmail.com>
2025-06-29 15:22:10 +0700

    ..

--- frontend/src/components/common/index.js
+++ frontend/src/components/common/index.js
@@ -1,3 +1,5 @@
 export { default as ErrorBoundary } from "./ErrorBoundary";
 export { default as LanguageSwitcher } from "./LanguageSwitcher";
 export { default as Loading } from "../admin/Loading";
+export {default as Toast} from "./Toast";
+export { default as ToastContainer } from "./ToastContainer";

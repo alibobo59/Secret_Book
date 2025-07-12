01aff3ebcfdaf89e54a6f9cc1814fcc17245b331
NguyenTrongTan-NNT <anhtan181004@gmail.com>
2025-07-12 21:10:40 +0700

    chore(seed): tạo khung skeleton cho CategoriesSeeder

---
+++ backend/database/seeders/CategoriesSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+/**
+ * -----------------------------------------------------------------------------
+ * CategoriesSeeder (skeleton)
+ * -----------------------------------------------------------------------------
+ * Seeder này sẽ chịu trách nhiệm thêm dữ liệu mẫu cho bảng `categories`.
+ *
+ * Hiện tại chỉ khởi tạo class và hàm run() trống.
+ * Logic seed thực sự sẽ được bổ sung ở commit tiếp theo.
+ */
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Chạy seeder.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        // TODO: thêm logic seed ở commit sau
+    }
+}
